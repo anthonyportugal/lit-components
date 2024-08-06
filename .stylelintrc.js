45106b0c64dacab4cb2be15c2f22928370847bad
Anthony Portugal <anthonyportugalrobles@gmail.com>
2024-08-06 14:39:40 -0500

    🔧 add stylelint config

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+/**
+ * @see https://stylelint.io/user-guide/configure/
+ * @type {import("stylelint").Config}
+ */
+export default {
+	extends: 'stylelint-config-standard-scss',
+	plugins: ['stylelint-scss']
+};
