5dcc409cbade760fe7755125ee97106323c9c749
Anthony Portugal <anthonyportugalrobles@gmail.com>
2024-08-06 17:37:04 -0500

    🔧 add eslint-config-prettier to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(pluginJs.configs.recommended, {
-	extends: [...tseslint.configs.recommended],
+	extends: [...tseslint.configs.recommended, eslintConfigPrettier],
 	files: ['**/*.ts'],
 	ignores: ['**/dist/**', '**/node_modules/**'],
 	languageOptions: {
